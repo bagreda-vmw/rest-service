0f2e61d5b7b80dfd8b72ae8dc6ddeeca2494f4af
Bart Agreda <114963139+bagreda-vmw@users.noreply.github.com>
2023-02-21 18:58:31 +0800

    Update GreetingControllerTests.java

--- src/test/java/com/example/restservice/GreetingControllerTests.java
+++ src/test/java/com/example/restservice/GreetingControllerTests.java
@@ -44,9 +44,9 @@ public class GreetingControllerTests {
 	@Test
 	public void paramGreetingShouldReturnTailoredMessage() throws Exception {
 
-		this.mockMvc.perform(get("/greeting").param("name", "Spring Community"))
+		this.mockMvc.perform(get("/greeting").param("name", "Tanzu!"))
 				.andDo(print()).andExpect(status().isOk())
-				.andExpect(jsonPath("$.content").value("Hello, Spring Community!"));
+				.andExpect(jsonPath("$.content").value("Hello, Tanzu!"));
 	}
 
 }
